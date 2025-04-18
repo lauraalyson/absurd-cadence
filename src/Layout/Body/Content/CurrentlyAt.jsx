a7fbdec190b56217901ba5e0de7ff56e02924ba4
lauraalyson <lauraalyson3@gmail.com>
2025-04-18 16:22:12 -0400

    Mobile style correction

--- src/Layout/Body/Content/CurrentlyAt.jsx
+++ src/Layout/Body/Content/CurrentlyAt.jsx
@@ -3,11 +3,17 @@ import { gigasheet_link } from "../../../assets/links"
 const CurrentlyAt = () => {
 	return (
 		<div className='font-semibold'>
-			<div className='text-xl pb-3'>
+			<div className='text-base md:text-xl  pb-3'>
 				Currently at{' '}
-				<a href={gigasheet_link} target='__blank' className='underline underline-offset-3 hover:underline-offset-2'>Gigasheet</a>
+				<a
+					href={gigasheet_link}
+					target='__blank'
+					className='underline underline-offset-3 hover:underline-offset-2'
+				>
+					Gigasheet
+				</a>
 			</div>
-			<div className='text-2xl py-3'>
+			<div className='text-xl md:text-2xl py-3'>
 				Crafting performant, AI-driven spreadsheets, built for scale.
 			</div>
 		</div>
--- src/Layout/Body/Content/PastWork.jsx
+++ src/Layout/Body/Content/PastWork.jsx
@@ -1,15 +1,17 @@
 const PastWork = () => {
 	return (
 		<div className='w-full font-semibold'>
-			<div className='text-xl pb-3'>To Do:</div>
-			<div className='font-semibold text-2xl border-b-2 py-3'>
+			<div className='text-base md:text-xl pb-3'>To Do:</div>
+			<div className='font-semibold text-xl md:text-2xl border-b-2 py-3'>
 				Software Development
 			</div>
-			<div className='font-semibold text-2xl border-b-2 py-3'>UX/UI Design</div>
-			<div className='font-semibold text-2xl border-b-2 py-3'>
+			<div className='font-semibold text-xl md:text-2xl border-b-2 py-3'>
+				UX/UI Design
+			</div>
+			<div className='font-semibold text-xl md:text-2xl  border-b-2 py-3'>
 				Product Research
 			</div>
-			<div className='font-semibold text-2xl py-3'>Etc.</div>
+			<div className='font-semibold text-xl md:text-2xl py-3'>Etc.</div>
 		</div>
 	)
 }
